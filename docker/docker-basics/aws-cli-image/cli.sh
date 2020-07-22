ed5b51ad584f4f537b290b5e490772aa79f48cd3
steveyttt <stevey_ttt@hotmail.co.uk>
2020-07-22 11:50:46 +1000

    Update cli.sh
    added region

--- docker/docker-basics/aws-cli-image/cli.sh
+++ docker/docker-basics/aws-cli-image/cli.sh
@@ -6,4 +6,4 @@ docker run --rm -it -v ~/.aws:/root/.aws --entrypoint sh amazon/aws-cli
 alias aws='docker run --rm -ti -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli:2.0.6'
 
 ## Pin the version and leverage env vars (Not file)
-alias aws='docker run --rm -ti -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli:2.0.6
+alias aws='docker run --rm -ti -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli:2.0.6
